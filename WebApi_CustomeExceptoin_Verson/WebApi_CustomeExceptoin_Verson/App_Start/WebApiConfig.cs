201823eb28d44fa355fefdc31b45eec695aacb92
Madan Shekar <madan.shekar@ad.starmarkit.com>
2019-06-06 19:33:10 +0530

    first commit

---
+++ WebApi_CustomeExceptoin_Verson/WebApi_CustomeExceptoin_Verson/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Web.Http.Versioning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using WebApi_CustomeExceptoin_Verson.CustomHandler;
+
+namespace WebApi_CustomeExceptoin_Verson
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            config.AddApiVersioning(o =>
+            {
+                o.ReportApiVersions = true;
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.DefaultApiVersion = new Microsoft.Web.Http.ApiVersion(2, 0);
+                o.ApiVersionReader = new HeaderApiVersionReader("version");
+                o.ApiVersionSelector = new CurrentImplementationApiVersionSelector(o);
+            }
+            );
+
+            config.MapHttpAttributeRoutes();
+            config.Services.Replace(typeof(IExceptionHandler), new GlobalExceptionHandler());
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApi_CustomeExceptoin_Verson/WebApi_CustomeExceptoin_Verson/Controllers/Values2Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Web.Http;
+namespace WebApi_CustomeExceptoin_Verson.Controllers
+{
+    public class Values2Controller : ApiController
+    {
+        [ApiVersion("2.0")]
+        [Route("api/values")]
+        public IEnumerable<string> Get()
+        {
+
+            return new string[] { "version 2.0", "version 2.0" };
+        }
+    }
+}
